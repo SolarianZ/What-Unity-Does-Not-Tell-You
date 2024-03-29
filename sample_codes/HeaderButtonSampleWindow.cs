a39288d7a8526f8c2e1cc60dfede49c781b61b5b
Qiuyu ZHANG <VDerGoW@hotmail.com>
2024-03-29 10:52:19 +0800

    Simplified code and corrected button style (removing the gray background of the button)

--- sample_codes/HeaderButtonSampleWindow.cs
+++ sample_codes/HeaderButtonSampleWindow.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class HeaderButtonSampleWindow : EditorWindow
 {
-    private GUIStyle _headerButtonStyle;
-
     [MenuItem("Samples/Header Button")]
     public static void Open() => GetWindow<HeaderButtonSampleWindow>("Header Button");
 
@@ -15,18 +13,8 @@ public class HeaderButtonSampleWindow : EditorWindow
     /// <param name="position"></param>
     private void ShowButton(Rect position)
     {
-        // Button style
-        if (_headerButtonStyle == null)
-        {
-            _headerButtonStyle = new GUIStyle(GUI.skin.button)
-            {
-                // Remove paddings
-                padding = new RectOffset()
-            };
-        }
-
-        // Draw a help button
-        if (GUI.Button(position, EditorGUIUtility.IconContent("_Help"), _headerButtonStyle))
+        // draw button
+        if (GUI.Button(position, EditorGUIUtility.IconContent("_Help"), GUI.skin.FindStyle("IconButton")))
         {
             Application.OpenURL("https://docs.unity3d.com/Manual/index.html");
         }
